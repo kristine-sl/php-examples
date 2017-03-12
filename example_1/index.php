beff2cf9215a0e8549fefbb82f56a664d9d297c8
Kristine Sundt Lorentzen <kristinesundtlorentzen@Kristines-MacBook-Pro.local>
2017-03-12 14:49:47 +0100

    Added loop with PHP-code to index.php

--- example_1/index.php
+++ example_1/index.php
@@ -1,5 +1,12 @@
 <!doctype html>
 <html>
 	<head></head>
-	<body><?php echo 'Hello' ?></body>
+	<body>
+		<?php $nums = [1,2,3]; ?>
+		<ul>
+		<?php foreach ($nums as $num) { ?>
+			<li>nr <?= $num ?></li>
+		<?php } ?>
+		</ul>
+	</body>
 </html>
